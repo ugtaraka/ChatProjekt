6b1f749ba95f9f4b29c362ecfcd24572dbd13cab
Simon <Simondenting@gmail.com>
2025-04-01 14:14:58 +0200

    Opdateret så der er forbindelse og respons, ikke kun binært

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ type PeerMap = Arc<Mutex<HashMap<usize, Tx>>>;
 
 #[tokio::main]
 async fn main() {
-    // Initialiser logning fra log4rs.yaml filen
+    // Initialiser logning
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     info!("Starter server...");
     println!("Starter server...");
@@ -32,11 +32,11 @@ async fn main() {
     let addr = format!("{}:{}", config.address, config.port);
     let socket_addr: SocketAddr = addr.parse().expect("Ugyldig adresse");
 
-    // Delt tilstand for tilsluttede klienter
+    // Delte datastrukturer til klientforbindelser
     let peers = PeerMap::new(Mutex::new(HashMap::new()));
     let peer_id_counter = Arc::new(Mutex::new(0));
 
-    // Server HTML-filen
+    // Server HTML
     let html = warp::path::end()
         .and(warp::fs::file("html/index.html"))
         .map(|file| {
@@ -53,26 +53,27 @@ async fn main() {
             ws.on_upgrade(move |socket| handle_websocket(socket, peers, peer_id_counter))
         });
 
-    // Kombiner ruter
+    // Kombiner ruter og start serveren
     let routes = html.or(ws_route);
-
-    // Start serveren
     warp::serve(routes).run(socket_addr).await;
 }
 
+// Hjælpefunktion til deling af peers
 fn with_peers(peers: PeerMap) -> impl Filter<Extract = (PeerMap,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || peers.clone())
 }
 
+// Hjælpefunktion til deling af peer-id-tæller
 fn with_peer_id_counter(peer_id_counter: Arc<Mutex<usize>>) -> impl Filter<Extract = (Arc<Mutex<usize>>,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || peer_id_counter.clone())
 }
 
-// Håndter WebSocket-forbindelser
+// WebSocket-håndtering
 async fn handle_websocket(ws: warp::ws::WebSocket, peers: PeerMap, peer_id_counter: Arc<Mutex<usize>>) {
     let (mut write, mut read) = ws.split();
     let (tx, mut rx) = mpsc::unbounded_channel();
 
+    // Tildel unikt ID til klienten
     let peer_id = {
         let mut id_counter = peer_id_counter.lock().unwrap();
         *id_counter += 1;
@@ -84,25 +85,39 @@ async fn handle_websocket(ws: warp::ws::WebSocket, peers: PeerMap, peer_id_count
         peers.insert(peer_id, tx.clone());
     }
 
-    // Opgave til at videresende beskeder fra kanalen til WebSocket
+    // Task til at sende beskeder til klienten
     tokio::spawn(async move {
         while let Some(msg) = rx.recv().await {
-            if write.send(warp::ws::Message::binary(msg.into_data())).await.is_err() {
+            let warp_msg = match msg {
+                TungsteniteMessage::Text(text) => warp::ws::Message::text(text),
+                TungsteniteMessage::Binary(bin) => warp::ws::Message::binary(bin),
+                _ => continue,
+            };
+            if write.send(warp_msg).await.is_err() {
                 break;
             }
         }
     });
 
-    // Læs og broadcast beskeder asynkront
+    // Læs beskeder fra klienten og broadcast til alle
     while let Some(result) = read.next().await {
         match result {
             Ok(msg) => {
-                if msg.is_text() || msg.is_binary() {
-                    let tungstenite_msg = TungsteniteMessage::binary(msg.into_bytes());
-                    let peers = peers.lock().unwrap();
-                    for peer in peers.values() {
-                        let _ = peer.send(tungstenite_msg.clone());
-                    }
+                let tungstenite_msg = if msg.is_text() {
+                    // Behold som tekst
+                    TungsteniteMessage::Text(msg.to_str().unwrap_or_default().to_string())
+                } else if msg.is_binary() {
+                    // Behold som binær
+                    TungsteniteMessage::Binary(msg.into_bytes())
+                } else {
+                    // Ignorer andre typer
+                    continue;
+                };
+
+                // Send til alle andre klienter
+                let peers = peers.lock().unwrap();
+                for peer in peers.values() {
+                    let _ = peer.send(tungstenite_msg.clone());
                 }
             }
             Err(e) => {
@@ -112,7 +127,7 @@ async fn handle_websocket(ws: warp::ws::WebSocket, peers: PeerMap, peer_id_count
         }
     }
 
-    // Fjern klienten fra peer-sættet
+    // Fjern klienten når forbindelsen lukkes
     let mut peers = peers.lock().unwrap();
     peers.remove(&peer_id);
 }
